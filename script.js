570198e868bf1635fde75e50683a14a369fc37ce
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-01-11 15:53:01 -0500

    Added CSS and a dummy script
    Initial commits

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function blowUpEverything() {
+    alert("Your computer will now explode because I told it to.");
+    $("body").innerHTML = "";
+}
+
+window.onload = function() {
+    $("button1").onclick = blowUpEverything;
+}
