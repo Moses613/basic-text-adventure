b3bd4063bcfde59dce873fa979cbb6773edaa4be
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-01-15 14:25:08 -0500

    Update files

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ function blowUpEverything() {
 window.onload = function() {
     $("button1").onclick = blowUpEverything;
 }
+
+/*TODO: Randomize the placeholder for the player's name, using names like
+Sir Egghead the Extortionist, Sir Graham the Laxative Creampuff, Sir Necromancer the Necromancer the Third Jr., Sir Bob the Electric Cow,
+Dangamere the Dreadfully Woe-Begone Donkey, Sir John "In-The-John" Hamburgerstein, Umbridge the Unbelievably-Fat*/
