d53a6a26dd36cad152995f6b17119369aa368bf7
Moses613 <44594121+Moses613@users.noreply.github.com>
2019-01-17 00:04:35 -0500

    Update script.js
    Created a way to get the inputted player's name, also, created a default name. Started creating Stage Two, and provided a way to attach event handlers to subsequent stages.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 "use strict";
 
-//Tracks progress through the game
-var tracker = 1;
+//Global variables
+var tracker = 1; //Tracks progress throughout the game
+var name = "Link"; //Provides a variable to hold the player's name, its default is a famous character's name
+var nameInputted = false; //This is false until the first stage has been passed, and a character name input has been received
 
 //Element getter thing
 function $(id) {
     return document.getElementById(id);
 }
 
-//Randomize the placeholder for the character name
-var randomizePlaceholder = function() {
+function randomizePlaceholder() {
     var numero = Math.floor((Math.random() * 10) + 1);
     var placeholderAttribute = "";
     
@@ -71,13 +72,20 @@ function gameOver() {
     $("body").innerHTML = updateGameOverMessage(tracker);
 }
 
-//Gets the html for the next stage of the game
-var updatePage = function(x) {
+//Creates and returns the html for the next stage of the game
+function updatePage(x) {
     var html = "";
     
     switch (x) {
-        case 1:         //Current stage
-            html = "This is stage two"; //Stage two html
+        case 1:         //Represents initial stage (starting homepage)
+            html = "<h1>Stage Two</h1>" + //Stage two html
+                "<h2>One exquisitely ordinary day, " + name +
+                " was taking a walk, when suddenly, the King appears!</h2>" + 
+                "<h3>" + name + " chooses to:</h3><br>" +
+                "<button id=\"correctAnswer\">Bow like an obedient pansy</button><button>Ignore him</button><button>Throw tomatoes</button>"; 
+            break;
+        case 2:
+            html = "This is stage three";
             break;
     }
     
@@ -86,13 +94,28 @@ var updatePage = function(x) {
 
 
 //Progresses game, called when player selects the right answer
-var progressGame = function(x) {
-    $("body").innerHTML = updatePage(x);
+function progressGame(x) {
+    alert("You made the right choice. Progressing to next stage...");
+    $("body").innerHTML = updatePage(x); //Loads new html for the next stage
+    
+    //Updates the newly loaded DOM with the requisite event handlers
+    $("correctAnswer").onclick = OnCorrectChoice;
+    //TODO: Wrong choice buttons
     tracker++;
 }
 
-//Calls progress() on click of the right answer
-var OnCorrectChoice = function() {
+//Calls progress() on click of the right answer, passing the tracker as an argument, also gets the player's name as input during the first stage
+function OnCorrectChoice() {
+    //Gets player name input: executes once
+    if (!nameInputted) {
+        if ($("name").value !== "") {
+            name = $("name").value;
+            //If inputted value is null, the character's name remains the default
+        }
+        nameInputted = true;
+    }
+    
+    //Progresses game, executes for all stages of the game
     progressGame(tracker);
 }
 
